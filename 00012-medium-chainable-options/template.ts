f87b33bd260d8b61696b52e558a6c4096cfaa8e0
asiter <1139933860@qq.com>
2022-09-28 18:02:10 +0800

    add:添加00012-medium-chainable-options

--- 00012-medium-chainable-options/template.ts
+++ 00012-medium-chainable-options/template.ts
@@ -1,4 +1,10 @@
-type Chainable<option = {}> = {
-    option<K extends string, V>(key: K, value: V): Chainable<option & { [S in K]: V }>;
-    get(): option
+type Key<K extends string, V, O> = K extends keyof O
+  ? O[K] extends V
+    ? { error: `Key ${K} already exists` }
+    : K
+  : K
+
+type Chainable<O = {}> = {
+  option<K extends string, V>(key: Key<K, V, O>, value: V): Chainable<Omit<O, K> & {[P in K]: V}>
+  get(): O
 }
