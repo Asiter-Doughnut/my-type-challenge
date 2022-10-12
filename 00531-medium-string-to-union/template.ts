8a494312603f51c8f87d7b4b7be335760653ea00
asiter <1139933860@qq.com>
2022-10-12 18:09:23 +0800

    update:刷个记录

--- 00531-medium-string-to-union/template.ts
+++ 00531-medium-string-to-union/template.ts
@@ -1,1 +1,1 @@
-type StringToUnion<T extends string> = T extends '' ? never : T extends `${infer Head}${infer Tail}` ? Head | StringToUnion<Tail> : never
+type StringToUnion<T extends string> =  T extends '' ? never : T extends `${infer Head}${infer Tail}` ? Head | StringToUnion<Tail> : never
